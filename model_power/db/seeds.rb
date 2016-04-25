6b68aa9946d415bf9db38e0bc97dc753bab3de0c
Ryuichi Tachibana <tachibana-ryuichi@ed.tmu.ac.jp>
2016-04-25 12:41:01 +0900

    seeds edit

--- model_power/db/seeds.rb
+++ model_power/db/seeds.rb
@@ -10,5 +10,5 @@ Result.create(qid: 1, uid: "sotaro", ans: "a")
 Result.create(qid: 2, uid: "sotaro", ans: "b")
 Result.create(qid: 2, uid: "Yamane", ans: "c")
 
-Question.create(a: "path_to_A", b:"path_to_B", c:"path_to_C", d:"path_to_D", z:"path_to_Z")
-Question.create(a: "path_to_A", b:"path_to_B", c:"path_to_C", d:"path_to_D", z:"path_to_Z_その2")
+Question.create(a: "1/1.jpg", b:"1/2.jpg", c:"1/3.jpg", d:"1/4.jpg", z:"1/a.jpg")
+Question.create(a: "2/1.jpg", b:"2/2.jpg", c:"2/3.jpg", d:"2/4.jpg", z:"2/a.jpg")
