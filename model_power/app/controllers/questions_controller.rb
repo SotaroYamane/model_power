5f79a476fedac80faae82077051abcbf33f41a2c
Ryuichi Tachibana <tachibana-ryuichi@ed.tmu.ac.jp>
2016-04-25 10:28:52 +0900

    indexの編集

--- model_power/app/controllers/questions_controller.rb
+++ model_power/app/controllers/questions_controller.rb
@@ -69,6 +69,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:qid, :1, :2, :3, :4, :a)
+      params.require(:question).permit(:qid, :a, :b, :c, :d, :z)
     end
 end
