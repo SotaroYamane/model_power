e98d67b2fa03aaef6909c0894b19d1a81a70cb1e
Ryuichi Tachibana <tachibana-ryuichi@ed.tmu.ac.jp>
2016-04-26 09:53:37 +0900

    edit_show_questions_view(bug_remains)

--- model_power/app/controllers/questions_controller.rb
+++ model_power/app/controllers/questions_controller.rb
@@ -1,10 +1,21 @@
 class QuestionsController < ApplicationController
+  require 'pp'
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   # GET /questions
   # GET /questions.json
   def index
     @questions = Question.all
+    x = 0
+    @answered_list = Result.find_by_sql(["select qid from results where uid = ?", @current_user.uid])
+    @answered = Hash.new
+    @answered_list.each do |ans|
+      @answered.store(x, ans.qid)
+      x += 1
+    end
+    pp @answered_list
+    pp @answered
+    pp "-----------------------------------------------------------"
   end
 
   # GET /questions/1
