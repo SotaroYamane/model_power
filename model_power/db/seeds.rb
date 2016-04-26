deaa0680c12ecc65854784adb7c6e228966a2cb6
Ryuichi Tachibana <tachibana-ryuichi@ed.tmu.ac.jp>
2016-04-26 16:46:16 +0900

    debug_seeds

--- model_power/db/seeds.rb
+++ model_power/db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 for num in [*1..10] do
-	if num <= 1 and num >= 3 then
+	if num >= 1 and num <= 3 then
 		Question.create(a: "#{num}/1.jpg", b:"#{num}/2.jpg", c:"#{num}/3.jpg", d:"#{num}/4.jpg", y:"服に最も合う帽子を選ぼう！", z:"#{num}/a.jpg")
-	if num <= 4 and num >= 9 then
+	elsif num >= 4 and num <= 9 then
 		Question.create(a: "#{num}/1.jpg", b:"#{num}/2.jpg", c:"#{num}/3.jpg", d:"#{num}/4.jpg", y:"モデルの服装に最も似合うバッグを選ぼう！", z:"#{num}/a.jpg")
 	else
 		Question.create(a: "#{num}/1.jpg", b:"#{num}/2.jpg", c:"#{num}/3.jpg", d:"#{num}/4.jpg", y:"左のバッグに最も似ているバッグを選ぼう！", z:"#{num}/a.jpg")
